723654193372939b72ad59f0cb11630f32ca8934
Qinyuan <lhkwqy@qq.com>
2025-08-27 15:31:33 +0800

    support the debug compile option
    Signed-off-by: Qinyuan <lhkwqy@qq.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
 #! /bin/bash
 
-if [[ -d "build" ]]; then
-    rm -rf build
-fi
+DEBUG_CONFIG=""
 
-meson setup build
+function configure-and-compile {
+    if [[ -d "build" ]]; then
+	rm -rf build
+    fi
 
-pushd build
-meson compile
-popd
+    meson setup build ${DEBUG_CONFIG}
+
+    pushd build
+    meson compile
+    popd
+}
+
+while getopts "d" opt; do
+    case $opt in
+	d)
+	    DEBUG_CONFIG="--buildtype=debug"
+	    ;;
+	\?)
+	    ;;
+    esac
+done
+configure-and-compile
